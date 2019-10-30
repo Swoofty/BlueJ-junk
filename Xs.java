75f88be2057210ff9a35089d21db23e443d79a2b
Gabe M <SweftyClouds@gmail.com>
2019-10-30 09:24:21 -0500

    Update Xs.java

--- Xs.java
+++ Xs.java
@@ -1,15 +1,15 @@
-import java.util.Scanner;
-
 public class Xs
 {
  public static void randomX(){
-   int y = 0;
+  int z = 0;
+  int y = 0;
    while (y < 16){
-     y = (int) (Math.random() * 19) + 1;
+     y = (int) (Math.random() * 19) + 5;
      String numberOfx = "x";
      System.out.println(numberOfx.repeat(y));
    }
  }
+
  public static void main(String[] args) { 
     randomX();
   }
