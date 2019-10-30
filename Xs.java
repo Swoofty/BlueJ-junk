a8379ee0f80f5ba563701ec13b9bf48eb6987053
Gabe M <SweftyClouds@gmail.com>
2019-10-30 09:34:49 -0500

    Update Xs.java

--- Xs.java
+++ Xs.java
@@ -1,16 +1,20 @@
 public class Xs
 {
  public static void randomX(){
+  String numberOfx = "";
   int z = 0;
   int y = 0;
    while (y < 16){
      y = (int) (Math.random() * 19) + 5;
-     String numberOfx = "x";
-     System.out.println(numberOfx.repeat(y));
    }
+   while(z <= y){
+     z++;
+     numberOfx += "x";
+   }
+  System.out.println(numberOfx);
  }
 
  public static void main(String[] args) { 
-    randomX();
-  }
+  randomX();
+ }
 }
