6189c386b5ef37db5596041a4a741a8d410fd16d
Gabe M. <SweftyClouds@gmail.com>
2020-01-21 10:18:52 -0600

    ahaha

--- Practice62.java
+++ Practice62.java
@@ -15,10 +15,29 @@ public class Practice62
         }
         return ((biggestValue - smallestValue) + 1);
     }
+    public static int countInRange(int[] testArray, int min, int max){
+        int totalElements = 0;
+        for(int x = 0; x < testArray.length; ++x){
+            if(testArray[x] >= min && testArray[x] <= max) ++totalElements;
+        }
+        return totalElements;
+    }
+    public static boolean isSorted(double[] testArray){
+        for(int x = 0; x < testArray.length - 1; ++x){
+            if(testArray[x] >= testArray[x + 1]) return false;
+        }
+        return true;
+    }
+    public static int reoccurringElement(int[] testArray){
+        
+    }
     
     public static void main(String[] args){
         int[] arrayTest1 = {4, 4, 8, 12, 3, 4, 8};
         System.out.println(lastIndexOf(arrayTest1, 3));
         System.out.println(range(arrayTest1));
+        System.out.println(countInRange(arrayTest1, 4, 12));
+        double[] arrayTest2 = {1.2, 2.6, 4.4, 5.3, 9.4, 43.7};
+        System.out.println(isSorted(arrayTest2));
     }
 }
